c074c7b384ffd0394fdfd21a208efb9c5378cdbb
Utkarsh630 <shivamsr1998@gmail.com>
2024-11-09 23:09:07 +0530

    added hamcrest dependency and hamcrest matchers/ assertions

--- src/test/java/E2ETestCases.java
+++ src/test/java/E2ETestCases.java
@@ -1,11 +1,17 @@
 import io.restassured.http.ContentType;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
 
 public class E2ETestCases {
-    String username = "Charles";
+    String username = "Charl";
     String password = "Abcdef@1234";
 
     @BeforeClass
@@ -25,10 +31,14 @@ public class E2ETestCases {
                 .then()
                 .log()
                 .all()
-                .statusCode(201);
+                .statusCode(201)
+                .body("userID",notNullValue())
+                .body("username",equalTo(username))
+                .body("books",equalTo(new ArrayList<>()));
+
     }
 
-    @Test
+    @Test(description = "check for existing user registration")
     public void userAlreadyExistsTest(){
         given()
                 .contentType(ContentType.JSON)
@@ -40,6 +50,11 @@ public class E2ETestCases {
                 .then()
                 .log()
                 .all()
-                .statusCode(406);
+                .statusCode(406)
+                .contentType(ContentType.JSON)
+                .body("code",equalTo("1204"))
+                .body("message",equalTo("User exists!"));
         }
+
+
 }
