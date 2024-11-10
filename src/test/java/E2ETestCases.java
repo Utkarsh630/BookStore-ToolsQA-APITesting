aa65f70c74726bac98b7d89f8d3aabec2249dd30
Utkarsh630 <shivamsr1998@gmail.com>
2024-11-11 00:38:36 +0530

    Added Test Cases for Generating token and Fetching Books (Get Books & Get Book By ISBN)

--- src/test/java/E2ETestCases.java
+++ src/test/java/E2ETestCases.java
@@ -6,12 +6,11 @@ import java.util.ArrayList;
 
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
 
 
 public class E2ETestCases {
-    String username = "Charl";
+    String username = "Dev";
     String password = "Abcdef@1234";
 
     @BeforeClass
@@ -37,7 +36,6 @@ public class E2ETestCases {
                 .body("books",equalTo(new ArrayList<>()));
 
     }
-
     @Test(description = "check for existing user registration")
     public void userAlreadyExistsTest(){
         given()
@@ -55,6 +53,76 @@ public class E2ETestCases {
                 .body("code",equalTo("1204"))
                 .body("message",equalTo("User exists!"));
         }
+    @Test(description = "Generate Token: Success- Validate Response Status, Headers, and Response Body")
+    public void generateTokenTest_Success(){
+            given()
+                    .contentType(ContentType.JSON)
+                    .body("{\"userName\":\"" +username+ "\", \"password\":\""+password+ "\"}")
+                    .when()
+                    .log()
+                    .all()
+                    .post("Account/v1/GenerateToken")
+                    .then()
+                    .log()
+                    .all()
+                    .statusCode(200)
+                    .contentType(ContentType.JSON)
+                    .body("token", notNullValue())
+                    .body("expires", notNullValue())
+                    .body("status", equalTo("Success"))
+                    .body("result",equalTo("User authorized successfully."));
+        }
+    @Test(description = "Generate Token: Failure- Validate Response Status, Headers, and Response Body")
+    public void generateTokenTest_Failure(){
+        given()
+                .contentType(ContentType.JSON)
+                .body("{\"userName\":\"" +username+ "\", \"password\":\""+password+ "\"}")
+                .when()
+                .log()
+                .all()
+                .post("Account/v1/GenerateToken")
+                .then()
+                .log()
+                .all()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("token", nullValue())
+                .body("expired", nullValue())
+                .body("status", equalTo("Failed"))
+                .body("result",equalTo("User authorization failed."));
+    }
+
+    @Test(description = "Get All Books: Validate Response Status, Headers, and Response Body")
+    public void getAllBooksTest(){
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .log()
+                .all()
+                .get("BookStore/v1/Books")
+                .then()
+                .log()
+                .all()
+                .statusCode(200)
+                .body("books",notNullValue());
+
+    }
+
+    @Test(description = "Get Book by ISBN: Validate Response Status, Headers, and Response Body")
+    public void getBookByISBNTest(){
+        given()
+                .contentType(ContentType.JSON)
+                .queryParam("ISBN","9781449325862")
+                .when()
+                .log()
+                .all()
+                .get("BookStore/v1/Book")
+                .then()
+                .log()
+                .all()
+                .statusCode(200);
+    }
+
 
 
 }
