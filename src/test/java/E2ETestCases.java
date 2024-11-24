44f689bfffb9850f65cd9c9262cd0a090258d7d2
Utkarsh630 <shivamsr1998@gmail.com>
2024-11-24 18:43:59 +0530

    Added Delete books, update books by ISBN and other tests

--- src/test/java/E2ETestCases.java
+++ src/test/java/E2ETestCases.java
@@ -1,4 +1,5 @@
 import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -10,17 +11,18 @@ import static org.hamcrest.Matchers.*;
 
 
 public class E2ETestCases {
-    String username = "Dev";
+    String username = "Devashe";
     String password = "Abcdef@1234";
-
+    String token="";
+    String userID="";
     @BeforeClass
     public void setup(){
         baseURI = "https://bookstore.toolsqa.com/";
     }
 
-    @Test(description = "User Registration API with authentication: Validate Response status, Headers, and Response Body")
+    @Test(priority = 0, description = "User Registration API with authentication: Validate Response status, Headers, and Response Body")
     public void registerUserTest(){
-        given()
+       userID = given()
                 .contentType(ContentType.JSON)
                 .body("{\"userName\":\"" +username+ "\", \"password\":\""+password+ "\"}")
                 .when()
@@ -33,7 +35,8 @@ public class E2ETestCases {
                 .statusCode(201)
                 .body("userID",notNullValue())
                 .body("username",equalTo(username))
-                .body("books",equalTo(new ArrayList<>()));
+                .body("books",equalTo(new ArrayList<>()))
+                .extract().path("userID");
 
     }
     @Test(description = "check for existing user registration")
@@ -53,9 +56,9 @@ public class E2ETestCases {
                 .body("code",equalTo("1204"))
                 .body("message",equalTo("User exists!"));
         }
-    @Test(description = "Generate Token: Success- Validate Response Status, Headers, and Response Body")
+    @Test(priority = 1,description = "Generate Token: Success- Validate Response Status, Headers, and Response Body")
     public void generateTokenTest_Success(){
-            given()
+         token= given()
                     .contentType(ContentType.JSON)
                     .body("{\"userName\":\"" +username+ "\", \"password\":\""+password+ "\"}")
                     .when()
@@ -70,9 +73,10 @@ public class E2ETestCases {
                     .body("token", notNullValue())
                     .body("expires", notNullValue())
                     .body("status", equalTo("Success"))
-                    .body("result",equalTo("User authorized successfully."));
+                    .body("result",equalTo("User authorized successfully."))
+                    .extract().path("token");
         }
-    @Test(description = "Generate Token: Failure- Validate Response Status, Headers, and Response Body")
+    @Test(  description = "Generate Token: Failure- Validate Response Status, Headers, and Response Body")
     public void generateTokenTest_Failure(){
         given()
                 .contentType(ContentType.JSON)
@@ -108,7 +112,7 @@ public class E2ETestCases {
 
     }
 
-    @Test(description = "Get Book by ISBN: Validate Response Status, Headers, and Response Body")
+    @Test(description = "Get Book by ISBN: Validate Response Status, Headers, and Response Body" )
     public void getBookByISBNTest(){
         given()
                 .contentType(ContentType.JSON)
@@ -123,6 +127,94 @@ public class E2ETestCases {
                 .statusCode(200);
     }
 
+    @Test(description = "Add list of Books into user's book collection: Validate Response Status, Headers, and Response Body", dependsOnMethods = {"registerUserTest","generateTokenTest_Success"})
+    public void addListofBooksTest(){
+        given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer " + token)
+                .body("{\n" +
+                        "  \"userId\": \""+userID+"\",\n" +
+                        "  \"collectionOfIsbns\": [\n" +
+                        "    {\n" +
+                        "      \"isbn\": \"9781449325862\"\n" +
+                        "    }\n" +
+                        "  ]\n" +
+                        "}")
+                .log().all()
+                .when()
+                .post("BookStore/v1/Books")
+                .then()
+                .log()
+                .all()
+                .statusCode(201)
+                .contentType(ContentType.JSON)
+                .body("books.isbn",hasItem("9781449325862"));
+    }
 
+    @Test(description = "Failure: Add list of Books into user's book collection: Validate Response Status, Headers, and Response Body", dependsOnMethods = {"registerUserTest","generateTokenTest_Success"})
+    public void addListofBooksTest_Failure(){
+        given()
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "  \"userId\": \""+userID+"\",\n" +
+                        "  \"collectionOfIsbns\": [\n" +
+                        "    {\n" +
+                        "      \"isbn\": \"9781449325862\"\n" +
+                        "    }\n" +
+                        "  ]\n" +
+                        "}")
+                .log().all()
+                .when()
+                .post("BookStore/v1/Books")
+                .then()
+                .log()
+                .all()
+                .statusCode(401)
+                .contentType(ContentType.JSON)
+                .body("code",equalTo("1200"))
+                .body("message",equalTo("User not authorized!"));
+    }
+
+    @Test(description = "update books list of user by userId and ISBN: Validate Response Status, Headers, and Response Body")
+    public void updateBookListByISBN(){
+        given()
+                .contentType(ContentType.JSON)
+                .header("Authorization","Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6ImRldmFzciIsInBhc3N3b3JkIjoiQWJjZGVmQDIyIiwiaWF0IjoxNzMyNDUyMzA4fQ.ePysNSFWYrteMgxe6iiR5ql4I80zpw1OV1R08dOiTvY")
+                .body("{\n" +
+                        "  \"userId\": \"22ada42d-61e5-4821-b138-c8c502ce152b\",\n" +
+                        "  \"isbn\": \"9781449325862\"\n" +
+                        "}")
+                .log()
+                .all()
+                .when()
+
+                .put("BookStore/v1/Books/9781449365035")
+                .then()
+                .log()
+                .all()
+                .statusCode(200)
+                .body("userId",equalTo("22ada42d-61e5-4821-b138-c8c502ce152b"))
+                .body("username",equalTo("devasr"))
+                .body("books.isbn[0]",equalTo("9781449325862"));
+    }
+
+    @Test(description = "Delete book from collection by ISBN: Validate Response Status, Headers, and Response Body")
+    public void deleteBookListByISBN(){
+        given()
+                .contentType(ContentType.JSON)
+                .header("Authorization","Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6ImRldmFzciIsInBhc3N3b3JkIjoiQWJjZGVmQDIyIiwiaWF0IjoxNzMyNDUzNDkyfQ.LhFL_eYLZekLkvOCi8rbsJg-JjY80MY4mJC34poqg7s")
+                .body("{\n" +
+                        "  \"isbn\": \"9781449325862\",\n" +
+                        "  \"userId\": \"22ada42d-61e5-4821-b138-c8c502ce152b\"\n" +
+                        "}")
+                .log()
+                .all()
+                .when()
+                .delete("BookStore/v1/Book")
+                .then()
+                .log()
+                .all()
+                .statusCode(204);
+    }
 
 }
