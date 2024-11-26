b8079dc87e3c62dc9e0ef43a82838c486ff9e0b4
Utkarsh630 <shivamsr1998@gmail.com>
2024-11-27 00:53:53 +0530

    Added Faker dependency to generate Fake data and refarctored code

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,5 +1,6 @@
 package base;
 
+import com.github.javafaker.Faker;
 import config.ConfigLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -9,6 +10,7 @@ import static io.restassured.RestAssured.baseURI;
 
 public class BaseTest {
     protected static final Logger logger = LogManager.getLogger(BaseTest.class);
+    protected Faker faker = new Faker();
     @BeforeClass
     public void setup(){
         logger.debug("Setting up Base URI!");
--- src/test/java/tests/E2ETestCases.java
+++ src/test/java/tests/E2ETestCases.java
@@ -3,24 +3,18 @@ package tests;
 import base.BaseTest;
 import config.ConfigLoader;
 import io.restassured.http.ContentType;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
 import java.util.ArrayList;
-
-import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 
 public class E2ETestCases extends BaseTest {
-    String username = "Devashe";
-    String password = "Abcdef@1234";
+    String username = faker.name().username();
+    String password = faker.internet().password(8,16,true,true);
     String token="";
     String userID="";
-    @Test(priority = 0, description = "User Registration API with authentication: Validate Response status, Headers, and Response Body")
+    @Test(description = "User Registration API with authentication: Validate Response status, Headers, and Response Body")
     public void registerUserTest(){
        userID = given()
                 .contentType(ContentType.JSON)
@@ -39,7 +33,7 @@ public class E2ETestCases extends BaseTest {
                 .extract().path("userID");
 
     }
-    @Test(description = "check for existing user registration")
+    @Test(priority = 1, description = "check for existing user registration")
     public void userAlreadyExistsTest(){
         given()
                 .contentType(ContentType.JSON)
@@ -56,7 +50,7 @@ public class E2ETestCases extends BaseTest {
                 .body("code",equalTo("1204"))
                 .body("message",equalTo("User exists!"));
         }
-    @Test(priority = 1,description = "Generate Token: Success- Validate Response Status, Headers, and Response Body")
+    @Test(priority = 2,description = "Generate Token: Success- Validate Response Status, Headers, and Response Body")
     public void generateTokenTest_Success(){
          token= given()
                     .contentType(ContentType.JSON)
@@ -76,7 +70,7 @@ public class E2ETestCases extends BaseTest {
                     .body("result",equalTo("User authorized successfully."))
                     .extract().path("token");
         }
-    @Test(  description = "Generate Token: Failure- Validate Response Status, Headers, and Response Body")
+    @Test(priority = 2, description = "Generate Token: Failure- Validate Response Status, Headers, and Response Body")
     public void generateTokenTest_Failure(){
         given()
                 .contentType(ContentType.JSON)
@@ -88,7 +82,7 @@ public class E2ETestCases extends BaseTest {
                 .then()
                 .log()
                 .all()
-                .statusCode(200)
+                .statusCode(400)
                 .contentType(ContentType.JSON)
                 .body("token", nullValue())
                 .body("expired", nullValue())
@@ -96,7 +90,7 @@ public class E2ETestCases extends BaseTest {
                 .body("result",equalTo("User authorization failed."));
     }
 
-    @Test(description = "Get All Books: Validate Response Status, Headers, and Response Body")
+    @Test(priority = 3, description = "Get All Books: Validate Response Status, Headers, and Response Body")
     public void getAllBooksTest(){
         given()
                 .contentType(ContentType.JSON)
@@ -112,7 +106,7 @@ public class E2ETestCases extends BaseTest {
 
     }
 
-    @Test(description = "Get Book by ISBN: Validate Response Status, Headers, and Response Body" )
+    @Test(priority = 4, description = "Get Book by ISBN: Validate Response Status, Headers, and Response Body" )
     public void getBookByISBNTest(){
         given()
                 .contentType(ContentType.JSON)
@@ -127,8 +121,8 @@ public class E2ETestCases extends BaseTest {
                 .statusCode(200);
     }
 
-    @Test(description = "Add list of Books into user's book collection: Validate Response Status, Headers, and Response Body", dependsOnMethods = {"registerUserTest","generateTokenTest_Success"})
-    public void addListofBooksTest(){
+    @Test(priority = 5, description = "Add list of Books into user's book collection: Validate Response Status, Headers, and Response Body", dependsOnMethods = {"registerUserTest","generateTokenTest_Success"})
+    public void addListOfBooksTest(){
         given()
                 .contentType(ContentType.JSON)
                 .header("Authorization", "Bearer " + token)
@@ -151,7 +145,7 @@ public class E2ETestCases extends BaseTest {
                 .body("books.isbn",hasItem("9781449325862"));
     }
 
-    @Test(description = "Failure: Add list of Books into user's book collection: Validate Response Status, Headers, and Response Body", dependsOnMethods = {"registerUserTest","generateTokenTest_Success"})
+    @Test(priority = 5, description = "Failure: Add list of Books into user's book collection: Validate Response Status, Headers, and Response Body", dependsOnMethods = {"registerUserTest","generateTokenTest_Success"})
     public void addListofBooksTest_Failure(){
         given()
                 .contentType(ContentType.JSON)
@@ -175,19 +169,19 @@ public class E2ETestCases extends BaseTest {
                 .body("message",equalTo("User not authorized!"));
     }
 
-    @Test(description = "update books list of user by userId and ISBN: Validate Response Status, Headers, and Response Body")
+    @Test(priority = 6, description = "update books list of user by userId and ISBN: Validate Response Status, Headers, and Response Body")
     public void updateBookListByISBN(){
         given()
                 .contentType(ContentType.JSON)
                 .header("Authorization","Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6ImRldmFzciIsInBhc3N3b3JkIjoiQWJjZGVmQDIyIiwiaWF0IjoxNzMyNDUyMzA4fQ.ePysNSFWYrteMgxe6iiR5ql4I80zpw1OV1R08dOiTvY")
-                .body("{\n" +
-                        "  \"userId\": \"22ada42d-61e5-4821-b138-c8c502ce152b\",\n" +
-                        "  \"isbn\": \"9781449325862\"\n" +
-                        "}")
+                .body("""
+                        {
+                          "userId": "22ada42d-61e5-4821-b138-c8c502ce152b",
+                          "isbn": "9781449325862"
+                        }""")
                 .log()
                 .all()
                 .when()
-
                 .put(ConfigLoader.getProperty("addBook")+"9781449365035")
                 .then()
                 .log()
@@ -198,15 +192,16 @@ public class E2ETestCases extends BaseTest {
                 .body("books.isbn[0]",equalTo("9781449325862"));
     }
 
-    @Test(description = "Delete book from collection by ISBN: Validate Response Status, Headers, and Response Body")
+    @Test(priority = 7, description = "Delete book from collection by ISBN: Validate Response Status, Headers, and Response Body")
     public void deleteBookListByISBN(){
         given()
                 .contentType(ContentType.JSON)
                 .header("Authorization","Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6ImRldmFzciIsInBhc3N3b3JkIjoiQWJjZGVmQDIyIiwiaWF0IjoxNzMyNDUzNDkyfQ.LhFL_eYLZekLkvOCi8rbsJg-JjY80MY4mJC34poqg7s")
-                .body("{\n" +
-                        "  \"isbn\": \"9781449325862\",\n" +
-                        "  \"userId\": \"22ada42d-61e5-4821-b138-c8c502ce152b\"\n" +
-                        "}")
+                .body("""
+                        {
+                          "isbn": "9781449325862",
+                          "userId": "22ada42d-61e5-4821-b138-c8c502ce152b"
+                        }""")
                 .log()
                 .all()
                 .when()
